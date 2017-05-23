82d360e5ea33e0bcce4debe13b54f3a8ddcf57d3
Xavier Perez <xap5xap@gmail.com>
2017-05-23 12:30:54 -0500

    about, home and coyrses page

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>About</h1>
+                <p>This app uses react, redux, react router, ....</p>
+            </div>
+        );
+    }
+}
+
+export default AboutPage;
